74270695d7b52a50269a9b53b9146232373aa7b5
Hiếu <hieudinhle1204@gmail.com>
2026-07-15 16:46:56 +0700

    feat(terraform): provision staging and production environments

---
+++ infra/live/staging/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = merge(var.additional_tags, {
+      ManagedBy   = "Terraform"
+      Project     = var.project_name
+      Repository  = "goldenowl-devops-internship-challenge"
+      Environment = "staging"
+      Stack       = "staging"
+    })
+  }
+}
